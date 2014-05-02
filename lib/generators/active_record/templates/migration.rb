56625a4fe84b65b0e8cdc070c833e1d1b68f4072
Dmitry Halai <dmitry.halai@gmail.com>
2014-05-02 12:23:12 +0300

    little fixes

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -5,8 +5,10 @@ class DeviseSmsActivableAddTo<%= table_name.camelize %> < ActiveRecord::Migratio
       t.string   :sms_confirmation_token, :limit => 15
       t.datetime :confirmation_sms_sent_at
       t.datetime :sms_confirmed_at
-      t.index    :sms_confirmation_token # for sms_activable
     end
+
+    add_index :<%= table_name %>, :phone, unique: true
+    add_index :<%= table_name %>, :sms_confirmation_token, unique: true
   end
   
   def self.down
